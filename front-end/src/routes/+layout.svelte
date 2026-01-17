1b6b5dcabedccd6fea97d83e9faab279db07e09a
Jaos <andresorozco1098@hotmail.com>
2026-01-17 13:10:04 -0600

    Elaborando Login en front-end

---
+++ front-end/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import { onMount } from "svelte";
+  onMount(async () => {
+    await import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  });
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ front-end/src/routes/login/+page.svelte
@@ -0,0 +1,48 @@
+<div
+  class="container min-vh-100 d-flex align-items-center justify-content-center"
+>
+  <div class="card shadow-sm w-100" style="max-width: 400px;">
+    <div class="card-body">
+      <h2 class="text-center staywell-purple">
+        Stay <span class="higlighted-purple"> Well</span>
+      </h2>
+      <div class="row">
+        <div class="col-12 mt-3 ps-5 pe-5">
+          <label for="" class="mb-3 staywell-purple">Usuario</label>
+          <br />
+          <input type="text" class="form-control" />
+        </div>
+        <div class="col-12 mt-3 ps-5 pe-5">
+          <label for="" class="mb-3 staywell-purple">Usuario</label>
+          <br />
+          <input type="text" class="form-control" />
+        </div>
+        <div class="col-12 mt-3 ps-5 pe-5">
+          <button class="btn btn-primary form-control mb-2">Entrar</button>
+          <br>
+          <button class="btn btn-outline-secondary form-control">Crear cuenta</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .higlighted-purple {
+    background-color: #7211f5;
+    color: white;
+    font-weight: 400;
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+
+  .staywell-purple {
+    color: #7211f5;
+  }
+
+
+  
+
+
+  
+</style>
